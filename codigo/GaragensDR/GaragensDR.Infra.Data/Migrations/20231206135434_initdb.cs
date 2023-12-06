d3b249b1586883d67d728b8dbe6bb1d305bd9015
Thiago Mateus <thiago.mateus@tlsistemas.com.br>
2023-12-06 10:55:58 -0300

    Adicionando migrations

---
+++ codigo/GaragensDR/GaragensDR.Infra.Data/Migrations/20231206135434_initdb.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GaragensDR.Infra.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initdb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FormasPagamento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Codigo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    DataCriacao = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "getdate()"),
+                    DataAtualizacao = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FormasPagamento", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Garagens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Codigo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Preco1aHora = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrecoHorasExtra = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrecoMensalista = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    DataCriacao = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "getdate()"),
+                    DataAtualizacao = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Garagens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Passagens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Garagem = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CarroPlaca = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CarroMarca = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CarroModelo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataHoraEntrada = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataHoraSaida = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FormaPagamento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrecoTotal = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false, defaultValue: true),
+                    DataCriacao = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "getdate()"),
+                    DataAtualizacao = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "getdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Passagens", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FormasPagamento");
+
+            migrationBuilder.DropTable(
+                name: "Garagens");
+
+            migrationBuilder.DropTable(
+                name: "Passagens");
+        }
+    }
+}
